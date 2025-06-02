0c27e6eb47b190aa196b7f89aecdbb4fb1175672
bitbyte08 <eight2823@naver.com>
2025-06-02 11:18:26 +0900

    feat :: HomeBanner 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,18 @@ import Image from "next/image";
 import Main from "@public/images/mainback.svg"
 import ContentContainer from "@/containers/main/ContentContainer";
 import NoticeContainer from "@/containers/main/NoticeContainer";
+import HomeBanner from "@/containers/main/HomeBanner";
 
 export default function Home() {
   return (
     <div>
       <ContentContainer>
         <Image src={Main} alt={"main"} className="absolute top-[-20px] z-0"/>
-        <div className="absolute top-1/2 -translate-y-1/2 z-1 opacity-50 bg-white h-[40rem] left-[10rem] right-[10rem] rounded-4xl">
-
-        </div>
+        <HomeBanner />
       </ContentContainer>
       <NoticeContainer />
       <ContentContainer>
-
+        <h1></h1>
       </ContentContainer>
     </div>
   );
