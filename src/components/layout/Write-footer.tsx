aca847dfc67b2c6499d7c47e57b92ded95ee17d8
dohun08 <bemik624@gmail.com>
2025-06-03 17:59:20 +0900

    feat(#3) :: write 용 layout 제작

---
+++ src/components/layout/Write-footer.tsx
@@ -0,0 +1,28 @@
+'use client'
+import React from "react";
+import { useRouter } from "next/navigation";
+import {useModalStore} from "@/store/modal";
+
+const FooterButton = ({children, bgColor, onClick}: Readonly<{children: React.ReactNode, bgColor : string , onClick?: () => void;}>) => {
+    return (
+        <button
+            onClick={onClick}
+            style={{ backgroundColor: bgColor, fontWeight : 800, color : bgColor === "#ffffff" ? "#ED9735" : "#ffffff" }}
+            className="cursor-pointer h-[2.5rem] w-[6.5rem] rounded-[0.625rem] border-2 border-[#ED9735] text-sm flex justify-center items-center"
+        >
+            {children}
+        </button>
+    )
+}
+export const WriteFooter = () => {
+  const {toggleModal} = useModalStore();
+    const router = useRouter();
+    return (
+        <footer className=" gap-4 h-[5.25rem] w-full fixed bottom-0 left-0 right-0 pl-15 pr-15 flex justify-end text-white items-center">
+            <FooterButton onClick={() => router.push("/")} bgColor={"#ffffff"}>돌아가기</FooterButton>
+            <FooterButton onClick={()=>toggleModal()} bgColor={"#ED9735"}>게시하기</FooterButton>
+        </footer>
+    )
+}
+
+
