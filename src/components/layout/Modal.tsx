04bcc336be33b38f763b8eebf9c4ce4440941489
dohun08 <bemik624@gmail.com>
2025-06-03 19:35:41 +0900

    feat(#3) :: 모달 에니메이션 추가

--- src/components/layout/Modal.tsx
+++ src/components/layout/Modal.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import "@/components/layout/animation.css"
 
-export default function Modal({ isOpen, children }: {
+export default function Modal({toggleModal, isOpen, children }: {
   isOpen: boolean,
+  toggleModal : () => void,
   children: React.ReactNode
 }) {
   if (!isOpen) return null;
 
   return (
-    <div className="w-screen fixed inset-0 bg-black/40 z-50 flex justify-center items-center">
-      <div className="w-[40%] min-h-[40%] bg-white p-12 rounded-xl shadow-lg relative">
+    <div onClick={toggleModal} className="w-screen fixed inset-0 bg-black/40 z-50 flex justify-center items-center">
+      <div
+        onClick={(e)=>e.stopPropagation()}
+        className="w-[40%] min-h-[40%] modal-enter bg-white p-12 rounded-xl shadow-lg relative"
+      >
         {children}
       </div>
     </div>
