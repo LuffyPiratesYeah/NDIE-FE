21806ab3fa58f54a401374f95d0c5d5ae83aa55f
dohun08 <bemik624@gmail.com>
2025-06-05 10:33:52 +0900

    fixed(#15) :: 로딩 컴포넌트 크기 변경

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -1,9 +1,8 @@
 export default function Loading() {
   return (
-    <div className="fixed top-0 left-0 flex justify-center items-center w-screen h-screen z-50">
+    <div className="fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
       <div
-        className="animate-spin rounded-full border-t-2 border-b-2 border-gray-900"
-        style={{ width: '10vw', height: '10vh' }}
+        className="animate-spin rounded-full w-[10vw] h-[10vw] border-t-2 border-b-2 border-gray-900"
       ></div>
     </div>
   );
