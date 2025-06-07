9528cff16b3e5db5797c8320d5d08b8136ccf13f
dohun08 <bemik624@gmail.com>
2025-06-07 18:12:06 +0900

    chore :: 주소변경

--- src/service/announcement.ts
+++ src/service/announcement.ts
@@ -1,6 +1,6 @@
 import axios from "@/lib/axiosInstance";
 
 export async function getAnnouncement() {
-  const response = await axios.get("http://ndie-be-985895714915.europe-west1.run.app/announcement");
+  const response = await axios.get("/announcement");
   return response.data;
 }
