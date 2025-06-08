9e7a864c1506f45a689baceb38349feb49e4e887
heodongun <162291579+heodongun@users.noreply.github.com>
2025-06-08 16:43:07 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig = {
   },
   env: {
     NEXT_PUBLIC_API_BASE: process.env.NEXT_PUBLIC_API_BASE,
+    NEXT_PUBLIC_KAKAO_BASE: process.env.NEXT_PUBLIC_KAKAO_BASE,
+    EMAIL_HOST: process.env.EMAIL_HOST,
+    EMAIL_PORT: process.env.EMAIL_PORT,
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASS: process.env.EMAIL_PASS,
   },
 };
 
